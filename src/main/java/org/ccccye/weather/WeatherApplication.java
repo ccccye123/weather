0d29572aa62657d92af94766b41e7de2306342c9
ccccye <w0.0.3@163.com>
2020-06-03 16:04:46 +0800

    test commit

--- src/main/java/org/ccccye/weather/WeatherApplication.java
+++ src/main/java/org/ccccye/weather/WeatherApplication.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class WeatherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeatherApplication.class, args);
+		SpringApplication.run(WeatherApplication.class, args); 
 	}
 
 }
